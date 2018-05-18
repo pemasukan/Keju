24bb651bf8adde9a601eb31b2df034594c2c30b0
imronsmdrj <imronsumadireza@gmail.com>
2018-05-18 22:42:27 +0700

    add milo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,request,Response,jsonify
-from peneliti import milo as ml
+from peneliti import milo as ml, ml
 import scholarly,json
 
 app = Flask(__name__)
@@ -20,4 +20,9 @@ def login():
 @app.route('/peneliti/<nama>')
 def apaajalah(nama):
     data = ml.cari(nama)
+    return jsonify(data)
+
+@app.route('peneliti/<nama>')
+def coba2(nama):
+    data = ml.coba(nama)
     return jsonify(data)
--- peneliti/milo.py
+++ peneliti/milo.py
@@ -1,3 +1,8 @@
 import scholarly
 def cari(nama):
-    return next(scholarly.search_author(nama))
+    return next(scholarly.search_author('Steven A. Cholewiak')).fill()
+    #print(nama)
+
+import scholarly
+def coba(nama):
+    return next(scholarly.search_author(nama)).fill()
